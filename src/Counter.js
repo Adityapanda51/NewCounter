30ebeda3626cc07ec753c78cc2405dfa1fa118ed
Aditya Kumar Panda <adityapanda15@gmail.com>
2024-06-07 22:47:42 +0530

    m

---
+++ src/Counter.js
@@ -0,0 +1,47 @@
+import React, { useState } from 'react';
+import { connect } from 'react-redux';
+import { increment, decrement } from './actions';
+import './Counter.css';
+
+const Counter = ({ count, increment, decrement }) => {
+  const [countColor, setCountColor] = useState('black'); // Initial color
+
+  const generateRandomColor = () => {
+    // Generate random RGB color values
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+    return `rgb(${r}, ${g}, ${b})`;
+  };
+
+  const handleIncrement = () => {
+    increment();
+    setCountColor(generateRandomColor()); // Set random color on increment
+  };
+
+  const handleDecrement = () => {
+    decrement();
+    setCountColor(generateRandomColor()); // Set random color on decrement
+  };
+
+  return (
+    <div>
+      <h1 className='heading'>Counter: <span style={{ color: countColor , marginLeft: '40px' }}>{count}</span></h1>
+      <div className='counterBtn'>
+        <button onClick={handleIncrement}>+</button>
+        <button onClick={handleDecrement}>-</button>
+      </div>
+    </div>
+  );
+};
+
+const mapStateToProps = state => ({
+  count: state.count
+});
+
+const mapDispatchToProps = {
+  increment,
+  decrement
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Counter);
